669f564310a7fae2c9d42ca48aa2656e97f5a3fd
Gladys Vilaro <glam-22@live.com>
2025-12-24 16:59:29 -0300

    Agregar carpeta docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/cotizador-hogar/', // 👈 nombre exacto de tu repo en GitHub
 })
+
