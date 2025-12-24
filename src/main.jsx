c44edfdbf2bff63c51a7e64149087e48bf8d9d6b
Gladys Vilaro <glam-22@live.com>
2025-12-24 17:56:09 -0300

    Subir build a docs para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// 👇 Agregamos basename para que funcione en GitHub Pages y en dev con /cotizador-hogar
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cotizador-hogar">
       <App />
     </BrowserRouter>
   </React.StrictMode>
