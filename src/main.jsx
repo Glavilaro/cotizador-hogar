e034cffefc5ef6452f64dc5e9a6dbc79ea0107ad
Gladys Vilaro <glam-22@live.com>
2025-12-24 18:15:38 -0300

    Build final con basename y rutas correctas

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// 👇 Agregamos basename para que funcione en GitHub Pages y en dev con /cotizador-hogar
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/cotizador-hogar">
